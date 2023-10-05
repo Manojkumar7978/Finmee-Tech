a6344db37011dcd349b9f4a27b6cc43ef28fca4d
Your Name <manojkumarpadhyms@gmail.com>
2023-10-05 21:56:38 +0530

    tooltip

--- github_clone/src/components/navbar.jsx
+++ github_clone/src/components/navbar.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 import {
     Box, chakra, Heading, InputGroup,
-
     Input,
     InputLeftElement,
     Button,
-    Avatar
+    Avatar,
+    Tooltip
 } from '@chakra-ui/react'
 import { HamburgerIcon, SearchIcon, AddIcon, TriangleDownIcon } from '@chakra-ui/icons'
 import { BsGithub } from 'react-icons/bs'
 import { GoIssueOpened, GoGitPullRequest } from 'react-icons/go'
 import { BiNotification } from 'react-icons/bi'
 
+
 export default function Navbar() {
     let navcontent = [
 
         {
             id: 'issues',
-            logo: GoIssueOpened
+            logo: GoIssueOpened,
+            display:['none','grid','grid'],
+            tooltip:'Issues'
         },
         {
             id: 'pull-request',
-            logo: GoGitPullRequest
+            logo: GoGitPullRequest,
+            display:['none','grid','grid'],
+            tooltip:'Pull requests'
         },
         {
             id: 'notification',
-            logo: BiNotification
+            logo: BiNotification,
+            display:'grid',
+            tooltip:'You have unread notifications'
         }
     ]
 
@@ -36,6 +43,7 @@ export default function Navbar() {
             display='flex'
             gap='8px'
             justifyContent='space-between' padding='16px' boxSizing='border-box' bg='#161b22' >
+           {/* navbar-leftside  */}
             <chakra.div id='nav-left' display='flex' w='full' alignItems='center' gap='8px'>
                 <chakra.div id='ham_menu' padding='6px' h={'32px'}
                     w={'32px'}
@@ -68,6 +76,7 @@ export default function Navbar() {
                     ><Heading size='xs' noOfLines={1} padding='6px'>Dashboard</Heading></chakra.a>
                 </chakra.div>
             </chakra.div>
+            {/* navbar-rightside  */}
             <chakra.div id='nav-right' display='flex'
                 gap='8px'
                 alignItems='center'
@@ -78,33 +87,39 @@ export default function Navbar() {
                     color='#8b949e'
                     size='sm'
                     cursor='pointer'
+                    display={['none','none','block']}
                 >
                     <InputLeftElement >
                         <SearchIcon color='#8b949e' />
                     </InputLeftElement>
-                    <Input w='350px' cursor='pointer' color='#8b949e' borderRadius='8px' border='1px solid #30363d ' placeholder='Type / to search' />
+                    <Input w='300px' cursor='pointer' color='#8b949e' borderRadius='8px' border='1px solid #30363d ' placeholder='Type / to search' />
 
                 </InputGroup>
-                <chakra.span>|</chakra.span>
-                <Button id='create-new' size='sm' bg='none'
+                <chakra.span display={['none','none','block']}>|</chakra.span>
+                <Tooltip bg='#6e7681' color='white' hasArrow label={'Create new...'} placement='bottom' borderRadius='6px' fontSize='11px'>
+                <Button id='create-new' display={['none','flex','flex']} size='sm' bg='none'
                     color='#8b949e'
 
                     border='1px solid #30363d'
                     _hover={{ border: '1px solid #8b949e ', bg: '#21262d' }}
                     rightIcon={<TriangleDownIcon w='9px' />} ><AddIcon /></Button>
 
+                </Tooltip>
                 {
                     navcontent.map((el, ind) => {
-                        return <chakra.div id={el.id} key={el.id} padding='6px' cursor='pointer' w='32px' h='32px'
-                            display='grid'
+                        return  <Tooltip bg='#6e7681' color='white' key={el.id} hasArrow label={el.tooltip} placement='bottom' borderRadius='6px' fontSize='11px'>  
+                        <chakra.div display={el.display} id={el.id}  padding='6px' cursor='pointer' w='32px' h='32px'
                             border='1px solid #30363d'
                             borderRadius='6px'
                             alignItems='center'
                             justifyContent='center'
                             _hover={{ border: '1px solid #8b949e ', bg: '#21262d' }}
                         >
+                           
                             <el.logo size={'18'} />
-                        </chakra.div>
+                           
+                        </chakra.div> 
+                        </Tooltip>
                     })
                 }
                 <Avatar id='avatar' cursor='pointer' size='sm' name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
